605e1820812eb168e53e118999cd88fbf32af37c
david-academy <david.andberg@gmail.com>
2019-06-25 17:10:20 +0300

    login ja jotain muuta

--- login.js
+++ login.js
@@ -2,12 +2,12 @@
 function check(form)/*function to check userid & password*/
 {
     /*the following code checks whether the entered userid and password are matching*/
-    if(form.userid.value == "myuserid" && form.pswrd.value == "mypswrd" || form.userid.value == "vieras" && form.pswrd.value =="vieras")
+    if(form.userid.value == "myuserid" && form.pswrd.value == "mypswrd" || form.userid.value == "vieras" && form.pswrd.value =="vieras" ||form.userid.value == "europe" && form.pswrd.value == "parasbändiikinä")
     {
         window.open('https://www.europetheband.com/')/*opens the target page while Id & password matches*/
     }
     else
     {
-        alert("Error Password or Username")/*displays error message*/
+        alert("Väärä salasana tai käyttäjänimi! \nKokeile esim. vieras/vieras")/*displays error message*/
     }
 }
