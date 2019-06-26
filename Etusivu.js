f7822e8bcb7ba5413537dfada975be28b265cee0
david-academy <david.andberg@gmail.com>
2019-06-26 16:57:43 +0300

    tågsökningens funktion fungerar i både login och på första sidan

--- Etusivu.js
+++ Etusivu.js
@@ -1,68 +1,15 @@
-var pyynto;
-var junat;
-document.addEventListener("DOMContentLoaded", init);
-function init() {
-    document.getElementById("nappi").addEventListener("click", hae);
-}
-function hae() {
-    pyynto = new XMLHttpRequest();
-    pyynto.onreadystatechange=tilanmuutos;
-    pyynto.open("GET", "https://rata.digitraffic.fi/api/v1/live-trains/");
-    pyynto.send();
-    console.log("Pyyntö lähetetty");
-}
-function tilanmuutos() {
-    console.dir(pyynto);
-    if (pyynto.readyState===4){
-        console.log(pyynto.responseText);
-        //document.getElementById("jotain").innerText=asemapyynto.responseText;
-        junat = JSON.parse(pyynto.responseText)
-        tulosta(junat);
-    }
-}
-function tulosta(junat) {
-    var junalista=document.getElementById("junalista")
-    for (var i=0; i<junat.length; i++){
-        var juna = junat[i];
-        console.dir(juna);
-        junalista.innerHTML+="<li>" + juna.trainCategory + ": " +  juna.trainType + juna.trainNumber +
-            ", lähtee Helsingistä raiteelta " + juna.timeTableRows[0].commercialTrack + " klo "
-            + juna.timeTableRows[0].scheduledTime.substring(11, 16) + " ja saapuu Lahteen klo "
-            + juna.timeTableRows[47].scheduledTime.substring(11, 16) +"</li>"
-    }
-
-}
-
-function check(form)/*function to check userid & password*/
-{
-    /*the following code checks whether the entered userid and password are matching*/
-    if(form.userid.value == "maja" && form.pswrd.value == "malli" || form.userid.value == "vieras" && form.pswrd.value == "vieras" ||form.userid.value == "europe" && form.pswrd.value == "parasbändiikinä")
-    {
-        localStorage.kirjautunutKayttaja=form.userid.value;
-        window.open(href="login.html")/*opens the target page while Id & password matches*/
-
-    }
-    else
-    {
-        alert("Väärä salasana tai käyttäjänimi! \nKokeile esim. vieras/vieras")/*displays error message*/
-    }
-}
 var asemapyynto; //pyyntö asemahaulle
 var asemat; //lista asemista -> saadaan lyhenteet junahakuun
 var lasema; //käyttäjän antama lähtöasema
 var perilla; //käyttäjän antama määränpää
 var junapyynto; //pyyntö junille kun asemat tiedossa
 var junat; // lista junista annetuilla asemilla
-var haetutjunat= []; //tallenetaan haettuja lähtöjä määränpää
 
-
-// junahaku
 document.addEventListener("DOMContentLoaded", init);
 function init() {
-    // document.getElementById("nappi").addEventListener("click", hae);
     hae();
 }
-
+//Haetaan asemat, joista käyttäjä saa valita lähtö- ja pääteaseman
 function hae() {
     asemapyynto = new XMLHttpRequest();
     asemapyynto.onreadystatechange = tilanmuutos;
@@ -70,7 +17,6 @@ function hae() {
     asemapyynto.send();
     console.log("Pyyntö lähetetty");
 }
-
 function tilanmuutos() {
     console.dir(asemapyynto);
     if (asemapyynto.readyState === 4) {
@@ -80,7 +26,6 @@ function tilanmuutos() {
         tulosta(asemat);
     }
 }
-
 function tulosta(asemat) {
     var asemalista = document.getElementById("asemat")
     for (var i = 0; i < asemat.length; i++) {
@@ -88,16 +33,14 @@ function tulosta(asemat) {
         console.dir(kaupunki);
         asemalista.innerHTML += "<option value= " + kaupunki.stationShortCode + ">" + kaupunki.stationName + "</option>"
     }
-
 }
-
+//Valitut asemat otetaan talteen, säilötään local storageen ja käytetään seuraavaan hakuun, jossa haetaan junat annettujen asemien välillä
 function naytaValitut() {
     lasema = document.getElementById("lahtoasema").value;
     localStorage.lahtoasema = lasema;
-    document.getElementById("tulos").innerHTML = "Lähtöasema oli " + lasema;
     perilla = document.getElementById("maaranpaa").value;
     localStorage.perilla = perilla;
-    document.getElementById("toinenkaupunki").innerHTML = "Määränpää on " + perilla;
+    document.getElementById("tulos").innerHTML="Junat välillä " + lasema + " - " + perilla + ":";
     haejunat();
 }
 
@@ -116,37 +59,172 @@ function junatilanmuutos() {
     if (junapyynto.readyState === 4) {
         console.log(junapyynto.responseText);
         //document.getElementById("jotain").innerText=asemapyynto.responseText;
-        junat = JSON.parse(junapyynto.responseText)
-        tulostajunat(junat);
-       document.getElementById("pnk2").style.display="block";
+        junat = JSON.parse(junapyynto.responseText);
+        if (junat.length > 0){
+            tulostajunat(junat);
+            document.getElementById("pnk2").style.display="block";
+
+        }else{
+            document.getElementById("tulos").innerHTML="Annettujen asemien välillä ei kulje suoraa junayhteyttä."
+        }
+
     }
 }
-
 function tulostajunat(junat) {
-    var junalista = document.getElementById("junalista")
-    for (var i = 0; i < junat.length; i++) {
+    var junataulukko=document.getElementById("junataulukko")
+    junataulukko.innerHTML="<tr>" + "<th>" + "Junatyyppi" + "<th>" + "Junan numero" + "<th>"+ "Lähtöpäivä" +"<th>" + "Lähtöaika" + "<th>" + "Saapumispäivä" + "<th>" + "Saapumisaika" + "</tr>";
+    for (var i=0; i< 5; i++){
         var juna = junat[i];
         console.dir(juna);
-        junalista.innerHTML += "<li>" + juna.trainCategory + ": " + juna.trainType + juna.trainNumber + "</li>"
+        var junatyyppi;
+        if (juna.trainCategory==="Commuter"){
+            junatyyppi= "Lähijuna " + juna.commuterLineID;
+        }else if (juna.trainCategory==="Long-distance"){
+            junatyyppi="Kaukojuna";
+        }else {
+            junatyyppi=juna.trainCategory;
+        }
+        //new Date oliolla tehdään datasta saadusta ajasta(CET) Suomen aikaa saapumisaika
+        junataulukko.innerHTML+="<tr>" + "<td>" + junatyyppi + "<td>" + juna.trainType + juna.trainNumber + "<td>" + new Date(etsiLahtoAika(juna.timeTableRows, lasema) ).toLocaleDateString('FI') +"<td>" + new Date(etsiLahtoAika(juna.timeTableRows, lasema) ).toLocaleTimeString('FI').slice(0, -3)
+            + "<td>" + new Date(etsiSaapumisAika(juna.timeTableRows, perilla)).toLocaleDateString('FI')+"<td>" + new Date(etsiSaapumisAika(juna.timeTableRows, perilla)).toLocaleTimeString('FI').slice(0, -3)+"</tr>";
     }
 
+
+
+    /*function tulostajunat(junat) {
+        var junalista = document.getElementById("junalista")
+
+        for (var i = 0; i < junat.length; i++) {
+            var juna = junat[i];
+            console.dir(juna);
+
+            var junatyyppi;
+            if (juna.trainCategory==="Commuter"){
+                junatyyppi= "Lähijuna " + juna.commuterLineID;
+            }else if (juna.trainCategory==="Long-distance"){
+                junatyyppi="Kaukojuna";
+            }else {
+                junatyyppi=juna.trainCategory;
+            }
+            junalista.innerHTML += "<li>" + junatyyppi + ": " + juna.trainType + juna.trainNumber + " lähtee " + etsiLahtoAika(juna.timeTableRows, lasema)
+                + " ja on perillä " + etsiSaapumisAika(juna.timeTableRows, perilla)+"</li>"
+
+        }*/
 }
+//haetaan junalistan aikatauluriveiltä rivi, jossa junan lähtöaika annetulta lähtöasemalta
+function etsiLahtoAika(timetablerows, asema) {
+    var tr;
+    tr=timetablerows.find(function (row){
+        return row.stationShortCode===asema && row.type==="DEPARTURE"; //palauttaa rivin, jossa annettu asemalyhenne ja lähtöaika ko asemalta
+    })
+    console.dir(tr);
+    return tr.scheduledTime;
+}
+
+//haetaan junalistan aikatauluriveiltä rivi, jossa junan saapumisaika annetulle määränpääasemalle
+function etsiSaapumisAika(timetablerows, asema) {
+    var tr;
+    tr=timetablerows.find(function(row){
+        return row.stationShortCode===asema && row.type==="ARRIVAL"; //palauttaa rivin, jossa annettu asemalyhenne ja saapumisaika ko asemalle
+    })
+    console.dir(tr);
+    return tr.scheduledTime
+
+}
+
+function haeKaikkiJunat() {
+    junapyynto = new XMLHttpRequest();
+    junapyynto.onreadystatechange = lyhytJunatilanmuutos;
+    junapyynto.open("GET", "https://rata.digitraffic.fi/api/v1/live-trains/station/" + lasema + "/" + perilla + "/");
+    junapyynto.send();
+    console.log("Pyyntö lähetetty");
+}
+
+function lyhytJunatilanmuutos() {
+    console.dir(junapyynto);
+    if (junapyynto.readyState === 4) {
+        console.log(junapyynto.responseText);
+        //document.getElementById("jotain").innerText=asemapyynto.responseText;
+        junat = JSON.parse(junapyynto.responseText);
+        tulostakaikkijunat(junat);
+
+
+    }
+}
+function tulostakaikkijunat(junat) {
+    var junataulukko = document.getElementById("kokojunataulukko")
+    junataulukko.innerHTML = "<tr>" + "<th>" + "Junatyyppi" + "<th>" + "Junan numero" + "<th>" + "Lähtöpäivä" + "<th>" + "Lähtöaika" + "<th>" + "Saapumispäivä" + "<th>" + "Saapumisaika" + "</tr>";
+    for (var i = 5; i < junat.length; i++) {
+        var juna = junat[i];
+        console.dir(juna);
+        var junatyyppi;
+        if (juna.trainCategory === "Commuter") {
+            junatyyppi = "Lähijuna " + juna.commuterLineID;
+        } else if (juna.trainCategory === "Long-distance") {
+            junatyyppi = "Kaukojuna";
+        } else {
+            junatyyppi = juna.trainCategory;
+        }
+        //new Date oliolla tehdään datasta saadusta ajasta(CET) Suomen aikaa saapumisaika
+        junataulukko.innerHTML += "<tr>" + "<td>" + junatyyppi + "<td>" + juna.trainType + juna.trainNumber + "<td>" + new Date(etsiLahtoAika(juna.timeTableRows, lasema)).toLocaleDateString('FI') + "<td>" + new Date(etsiLahtoAika(juna.timeTableRows, lasema)).toLocaleTimeString('FI').slice(0, -3)
+            + "<td>" + new Date(etsiSaapumisAika(juna.timeTableRows, perilla)).toLocaleDateString('FI') + "<td>" + new Date(etsiSaapumisAika(juna.timeTableRows, perilla)).toLocaleTimeString('FI').slice(0, -3) + "</tr>";
+    }
+
+
+//haetaan junalistan aikatauluriveiltä rivi, jossa junan lähtöaika annetulta lähtöasemalta
+    function etsiLahtoAika(timetablerows, asema) {
+        var tr;
+        tr = timetablerows.find(function (row) {
+            return row.stationShortCode === asema && row.type === "DEPARTURE"; //palauttaa rivin, jossa annettu asemalyhenne ja lähtöaika ko asemalta
+        });
+        console.dir(tr);
+        return tr.scheduledTime;
+    }
+
+//haetaan junalistan aikatauluriveiltä rivi, jossa junan saapumisaika annetulle määränpääasemalle
+    function etsiSaapumisAika(timetablerows, asema) {
+        var tr;
+        tr = timetablerows.find(function (row) {
+            return row.stationShortCode === asema && row.type === "ARRIVAL"; //palauttaa rivin, jossa annettu asemalyhenne ja saapumisaika ko asemalle
+        });
+        console.dir(tr);
+        return tr.scheduledTime
+
+    }
+}
+
+
+var haetutjunat= []; //tallenetaan haettuja lähtöjä määränpää
+
 const lisaaHaettu = (ev)=>{ //haetut junat arraylistana
     ev.preventDefault();
     var juna = {
         id: Date.now(),
         lahto: document.getElementById("lahtoasema").value,
         maaranpaa: document.getElementById("maaranpaa").value
-    }
+    };
     haetutjunat.push(juna);
     document.forms[0].reset();
     localStorage.setItem('Kayttajanhakemat', JSON.stringify(haetutjunat));
-}
+};
 document.addEventListener('DOMContentLoaded', ()=>{
     document.getElementById('pnk').addEventListener('click', lisaaHaettu)
 });
 
+function check(form)/*function to check userid & password*/
+{
+    /*the following code checks whether the entered userid and password are matching*/
+    if(form.userid.value == "maja" && form.pswrd.value == "malli" || form.userid.value == "vieras" && form.pswrd.value == "vieras" ||form.userid.value == "europe" && form.pswrd.value == "parasbändiikinä")
+    {
+        localStorage.kirjautunutKayttaja=form.userid.value;
+        window.location.href="login.html"/*opens the target page while Id & password matches*/
 
+    }
+    else
+    {
+        alert("Väärä salasana tai käyttäjänimi! \nKokeile esim. vieras/vieras")/*displays error message*/
+    }
+}
 
 
 
